e9cd9a4edb282cbe7bd37a718fbcb26a0fd4a169
EC2 Default User <ec2-user@ip-172-31-24-199.us-east-2.compute.internal>
2019-10-09 21:22:46 +0000

    page title and icon

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById("getTacoButton").addEventListener("click", function(event) {
     event.preventDefault();
-    document.getElementById("taco_info").innerHTML="<p>Got one!</p>";
     const url = "http://taco-randomizer.herokuapp.com/random/";
     fetch(url)
     .then(function(response) {
@@ -18,7 +17,7 @@ document.getElementById("getTacoButton").addEventListener("click", function(even
         results += "<p style='text-align: center; border: 3px #465E16 solid; padding: 10px; margin-right: 300px; margin-left: 300px; margin-bottom: 50px;'>" + json.seasoning.recipe + "</p>";
         results += "<h2 style='text-align: center;'>Condiment: </h2>" + "<h3>" + json.condiment.name + "</h3>";
         results += "<p style='text-align: center; border: 3px #465E16 solid; padding: 10px; margin-right: 300px; margin-left: 300px; margin-bottom: 50px;'>" + json.condiment.recipe + "</p>";
-        results += "<h2 style='text-align: center; margin-bottom: 50px;'>Enjoy your Taco! " + "<img src='images/dancingtaco.gif' width='50px'>" + "</h2>";
+        results += "<h2 style='text-align: center; margin-bottom: 50px;'>Enjoy your Taco!" + "</h2>";
         document.getElementById("taco_info").innerHTML = results;
     });
 });
