74fc3ca94b25a01b9eef93817423d6d440b717a8
EC2 Default User <ec2-user@ip-172-31-24-199.us-east-2.compute.internal>
2019-10-09 20:59:54 +0000

    commiting to pull

--- script.js
+++ script.js
@@ -17,27 +17,7 @@ document.getElementById("getTacoButton").addEventListener("click", function(even
         results += '<h2>Seasoning: ' + json.seasoning.name + '</h2>';
         results += '<p>' + json.seasoning.recipe + '</p>';
         results += '<h2>Condiment: ' + json.condiment.name + '</h2>';
-        results += '<p>' + json.condiment.recipe + '</p>';
+        results += '<p>' + json.condiment.recipe + '</p>';s
         document.getElementById("taco_info").innerHTML = results;
-        
-    //   let results = "";
-    //   results += '<h2>Weather in ' + json.name + "</h2>";
-    //   for (let i=0; i < json.weather.length; i++) {
-	   // results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-    //   }
-    //   results += '<h2>' + json.main.temp + " &deg;F</h2>"
-    //   results += "<p>"
-    //   for (let i=0; i < json.weather.length; i++) {
-	   // results += json.weather[i].description
-	   // if (i !== json.weather.length - 1)
-	   //     results += ", "
-    //   }
-    //   results += "</p>";
-    //   results += "<br><p>Humidity: " + json.main.humidity + "%</p>";
-    //   results += "<p>Cloud Cover: " + json.clouds.all + "%</p>";
-    //   results += "<p>Wind Speed: " + json.wind.speed + " mph</p>";
-    //   results += "<p>Sunrise: " + moment(json.sys.sunrise).format('h:mm a') + "</p>";
-    //   results += "<p>Sunset: " + moment(json.sys.sunset).format('h:mm a') + "</p>";
-    //   document.getElementById("weatherResults").innerHTML = results;
     });
 });
